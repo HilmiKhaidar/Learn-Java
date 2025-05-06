7ecaa82b9b9475746d22aa7c977d5fa426c7470c
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-06 03:00:44 -0700

    Modul 3 : VariableTipeData

---
+++ VariabelTipeData.java
@@ -0,0 +1,15 @@
+public class VariabelTipeData {
+    public static void main(String[] args) {
+        int umur = 20;
+        double tinggi = 170.5;
+        char golonganDarah = 'O';
+        boolean isMahasiswa = true;
+        String nama = "Mikhaii";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+        System.out.println("Golongan Darah: " + golonganDarah);
+        System.out.println("Mahasiswa? " + isMahasiswa);
+    }
+}
