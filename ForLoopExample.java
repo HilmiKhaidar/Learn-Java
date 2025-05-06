4fa162dab9a1bcdf8044bd774443acadaf61d611
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-06 03:53:13 -0700

    Modul 7 : For, while, Do While

---
+++ ForLoopExample.java
@@ -0,0 +1,9 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 5; i++) {
+            total += i;  // Menambahkan nilai i ke total
+        }
+        System.out.println("Total: " + total);
+    }
+}
