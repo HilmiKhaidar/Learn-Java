19fca3878c9a7eba707bd6bbef295e3718006c75
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-06 03:15:23 -0700

    Modul 5 : Operator Aritmatika

---
+++ OperatorAritmatika.java
@@ -0,0 +1,12 @@
+public class OperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        System.out.println("Penjumlahan: " + (a + b));
+        System.out.println("Pengurangan: " + (a - b));
+        System.out.println("Perkalian: " + (a * b));
+        System.out.println("Pembagian: " + (a / b)); // hasil int
+        System.out.println("Sisa bagi: " + (a % b));
+    }
+}
