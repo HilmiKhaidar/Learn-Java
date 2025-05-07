d62f9fba78ff40132ed927b0be8d76658d512941
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-07 10:19:50 -0700

    Modul 10 : Array 2 dimensi ( Matrix )

---
+++ PenjumlahanMatriks.java
@@ -0,0 +1,31 @@
+public class PenjumlahanMatriks {
+    public static void main(String[] args) {
+        int[][] matriksA = {
+            {1, 2},
+            {3, 4}
+        };
+
+        int[][] matriksB = {
+            {5, 6},
+            {7, 8}
+        };
+
+        int[][] hasil = new int[2][2];
+
+        // Proses penjumlahan
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                hasil[i][j] = matriksA[i][j] + matriksB[i][j];
+            }
+        }
+
+        // Tampilkan hasil
+        System.out.println("Hasil Penjumlahan Matriks:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(hasil[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
