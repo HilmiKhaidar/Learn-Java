7f72d4102b886abfc4f7ae2769ad7db7a18d6170
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-05-11 03:33:06 -0700

    Inheritance

---
+++ Inheritance/Kendaraan.java
@@ -0,0 +1,8 @@
+public class Kendaraan {
+    String merk;
+    int tahun;
+
+    void nyalakanMesin() {
+        System.out.println("Mesin " + merk + " tahun " + tahun + " dinyalakan.");
+    }
+}
---
+++ Inheritance/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Motor m = new Motor();
+        m.merk = "Honda";
+        m.tahun = 1990;
+        m.tipe = "GL100 Scrambler";
+
+        m.tampilInfo();
+        m.nyalakanMesin();
+    }
+}
